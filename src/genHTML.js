4f979caf8236ff13042d57e4f7506a947bf1f64a
Fatalis <connman2400@gmail.com>
2022-12-04 15:44:42 -0600

    added create engineer card

--- src/genHTML.js
+++ src/genHTML.js
@@ -1,4 +1,4 @@
-const createManager = function(manager){
+const createManagerCard = function(manager){
     return`
     <div class="col-4 mt-4">
         <div class="card h-100">
@@ -11,6 +11,25 @@ const createManager = function(manager){
                 <p>ID: ${manager.id}</p>
                 <p>Email: <a href="${manager.email}">${manager.email}</a></p>
                 <p>Office Number: ${manager.officeNum}</p>
+            </div>
+
+        </div>
+    </div>
+    `
+}
+const createEngineerCard = function(engineer){
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer}</h3>
+                <h4>Engineer</h4><i class="material-icons">support_agent</i>
+            </div>
+
+            <div class="card-body">
+                <p>ID: ${engineer.id}</p>
+                <p>Email: <a href="${engineer.email}">${engineer.email}</a></p>
+                <p>Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
             </div>
 
         </div>
