2c334e7d99dae181c3d4e2f6b2ceceddcff60f13
Fatalis <connman2400@gmail.com>
2022-12-17 19:03:53 -0600

    added prompt to generate employee data

--- index.js
+++ index.js
@@ -32,3 +32,53 @@ const newManager = () =>{
         staffArray.push(manager); 
     })
 }
+
+const newStaff = () =>{
+    return inquirer.prompt([
+        {
+            type:'list',
+            name:'role',
+            message:'select a role',
+            choices:['Intern','Engineer']
+        },{
+            type:'input',
+            name:'name',
+            message:'Enter a name'
+        },{
+            type:'input',
+            name:'id',
+            message:'Enter ID'
+        },{
+            type:'input',
+            name:'email',
+            message:'enter employee email'
+        },{
+            type:'input',
+            name:'github',
+            message:'Enter employee github username',
+            when:(input)=>input.role ==='Engineer'
+        },{
+            type:'input',
+            name:'school',
+            message:'Enter employee school',
+            when:(input)=>input.role ==='Intern'
+        },{
+            type:'confirm',
+            name:'anotherOne',
+            message:'add another employee?',
+            default: false
+        }
+    ]).then(staffData =>{
+        let {name,id,email,role,school,github,anotherOne} =staffData;
+        let employee;
+        if (role ==='Intern'){
+            employee = new Intern (name,id,email,school);
+        } else if (role ==='Engineer'){
+            employee = new Engineer(name,id,email,github);
+        }
+        staffData.push(employee);
+        if(anotherOne){
+            newStaff(); 
+        } 
+    })
+}
